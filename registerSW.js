ad4029212f3902fddf48b5cea08140ac48f694b1
Benjaminmusic <46501027+Benjaminmusic@users.noreply.github.com>
2026-07-26 04:45:09 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Credible-Resources-Africa/sw.js', { scope: '/Credible-Resources-Africa/' })})}
